de0c75ef73aa7b422dc5444103307b23b79f8085
VitaliiLitvinchuk <145115691+VitaliiLitvinchuk@users.noreply.github.com>
2026-03-19 15:34:44 +0000

    Deploying to gh-pages from @ VitaliiLitvinchuk/deployment-labs@e6a50fc155766737e7ea0b1757ae1e4e83bdac2a 🚀

--- _next/static/pKOYFlWMHB-mwY20UwWKf/_buildManifest.js
+++ _next/static/pKOYFlWMHB-mwY20UwWKf/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/deployment-labs//_next/:path+",
+        "destination": "/deployment-labs/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
