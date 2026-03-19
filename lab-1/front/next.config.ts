4a689ea84072be869a1596b4472cb80676d8fd36
Vitalii Litvinchuk <vitaliilitvinchuk@MacBookPro.lan>
2026-03-19 17:04:24 +0200

    fix: failed

--- lab-1/front/next.config.ts
+++ lab-1/front/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/deployment-labs',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
