cc981b032792d8c7c5663ff3ff88543ddef2b495
Vitalii Litvinchuk <vitaliilitvinchuk@MacBookPro.lan>
2026-03-30 09:27:16 +0300

    fix

--- lab-1/front/next.config.ts
+++ lab-1/front/next.config.ts
@@ -4,8 +4,8 @@ const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
 const nextConfig: NextConfig = {
   output: isGithubActions ? 'export' : undefined,
-  basePath: './',
-  assetPrefix: './',
+  basePath: isGithubActions ? '/deployment-labs' : '',
+  assetPrefix: isGithubActions ? '/deployment-labs/' : '',
   images: {
     unoptimized: true,
   },
